b6ba70a3d7fd30081a18eef76d90a7dfb66adbb6
wad-sh <tawjehiwadee@gmail.com>
2026-07-13 18:06:18 +0300

    1st real commit

---
+++ theapp/database/database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "sqlite:///./projdata.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args= {"check_same_thread" : False}
+)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False
+)
+
+Base = declarative_base()
